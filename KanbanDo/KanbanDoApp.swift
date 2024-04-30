16005f4c5ebfd9e43ab185deda8d582e6e2315f5
Seunghye Hwang <ferrydraw@naver.com>
2024-04-30 17:48:24 +0900

    Initial KanbanDo

---
+++ KanbanDo/KanbanDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanbanDoApp.swift
+//  KanbanDo
+//
+//  Created by 황승혜 on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct KanbanDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
