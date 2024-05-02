97feeb82685cbece9959cc705b339ef66dc3aeb5
khyerim <ax1021@naver.com>
2024-05-02 15:10:38 +0900

    deadline

--- taskCreationView/taskCreationView/ContentView.swift
+++ taskCreationView/taskCreationView/ContentView.swift
@@ -15,6 +15,13 @@ struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @Query(sort: \Task.text) var tasks: [Task]
     
+    //마감일 출력 양식
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        return formatter
+    }
+    
     //태스크 저장소
     var body: some View {
         GeometryReader { geometry in
@@ -26,13 +33,11 @@ struct ContentView: View {
                             Text("\(task.title)")
                                 .font(.title)
                             
-//                            Text("\(task.deadline)")
-//                                .font(.caption)
+                            Text("\(task.deadline, formatter: dateFormatter)")
+                                .font(.caption)
                             
                             Text("\(task.personCharge)")
                                 .font(.caption)
-                            
-                            Spacer()
                         }
                         .padding()
                         .background(.gray)
@@ -99,7 +104,7 @@ struct taskAddView: View {
 //    @Binding var showSheet: Bool
     @State var taskTitle: String = ""
     @State var taskText: String = ""
-    @State var taskDeadline = Date()
+    @State private var taskDeadline = Date()
     @State var taskStatus: Color =  .gray
     @State var taskPersonCharge: String = ""
     
@@ -108,83 +113,85 @@ struct taskAddView: View {
     var body: some View {
         VStack {
             // 상태 선택창
-            VStack {
+            ScrollView {
                 VStack(alignment: .leading) {
-                    Text("상태")
-                        .font(.title3)
-                        .fontWeight(.bold)
+                    VStack(alignment: .leading) {
+                        Text("상태")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        HStack {
+                            //todo: 상태 선택 버튼
+                        }
+                    }
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 30, trailing: 0))
+                    
+                    
+                    VStack(alignment: .leading) {
+                        Text("할 일")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        TextField("할 일을 적어주세요", text: $taskTitle, axis: .vertical)
+                            .frame(width: 320, height: 25)
+                            .padding(16)
+                            .border(Color.gray.opacity(0.4), width: 1)
+                            .cornerRadius(3.0)
+                    }
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 30, trailing: 0))
+                    
+                                        
+                    VStack(alignment: .leading) {
+                        Text("세부 내용")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        TextField("세부 내용을 적어주세요", text: $taskText, axis: .vertical)
+                            .frame(width: 320, height: 100)
+                            .padding(16)
+                            .border(Color.gray.opacity(0.4), width: 1)
+                            .cornerRadius(3.0)
+                    }
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 30, trailing: 0))
+                    
+                    
+                    VStack(alignment: .leading) {
+                        //todo: 마감일 선택
+                        DatePicker("마감일", selection: $taskDeadline, in: Date()..., displayedComponents: .date)
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        //.datePickerStyle(GraphicalDatePickerStyle()).labelsHidden()
+                        
+                    }
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 30, trailing: 0))
+                    
+                    
+                    VStack(alignment: .leading) {
+                        Text("담당자")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        //todo: 프로젝트에 포함된 사람 등록하는 기능
+                    }
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 100, trailing: 0))
+                }
+                
+                //todo: 위치/모양 바꾸기
+                NavigationLink {
+                    ContentView()
+                } label: {
                     HStack {
-                        //todo: 상태 선택 버튼
+                        Image(systemName: "Checkmark")
+                        Text("완료")
                     }
                 }
-                
-                Spacer()
-                
-                VStack(alignment: .leading) {
-                    Text("할 일")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                    TextField("할 일을 적어주세요", text: $taskTitle, axis: .vertical)
-                        .frame(width: 320, height: 30)
-                        .padding(16)
-                        .border(Color.gray.opacity(0.4), width: 1)
-                        .cornerRadius(3.0)
-                }
-                
-                Spacer()
-                
-                VStack(alignment: .leading) {
-                    Text("세부 내용")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                    TextField("세부 내용을 적어주세요", text: $taskText, axis: .vertical)
-                        .frame(width: 320, height: 30)
-                        .padding(16)
-                        .border(Color.gray.opacity(0.4), width: 1)
-                        .cornerRadius(3.0)
-                }
-                
-                Spacer()
-                
-                VStack(alignment: .leading) {
-                    Text("마감일")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                    //todo: 마감일 선택
-                    DatePicker("", selection: $taskDeadline, in: Date()...)
-                        .datePickerStyle(GraphicalDatePickerStyle()).labelsHidden()
-                }
-                
-                Spacer()
-                
-                VStack(alignment: .leading) {
-                    Text("담당자")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                    //todo: 프로젝트에 포함된 사람 등록하는 기능
-                }
-                
-                Spacer()
             }
-            
-            //todo: 위치/모양 바꾸기
-            NavigationLink {
-                ContentView()
-            } label: {
-                HStack {
-                    Image(systemName: "Checkmark")
-                    Text("완료")
-                }
+        
+            Button("완료") {
+                addTask(taskTitle, taskText, taskPersonCharge, color: taskStatus)
             }
-//            Button("완료") {
-//                addTask(taskTitle, taskText, taskPersonCharge, color: taskStatus)
-////                    showSheet = false
-//            }
-//            .disabled(taskTitle.isEmpty)
-//            .disabled(taskText.isEmpty)
-//           .disabled(taskPersonCharge.isEmpty)
+            .disabled(taskTitle.isEmpty)
+            .disabled(taskText.isEmpty)
+            //.disabled(taskPersonCharge.isEmpty)
         }
         .padding(EdgeInsets(top: 0, leading: 24, bottom: 0, trailing: 24))
+        .scrollIndicators(.hidden, axes: .vertical)
     }
     
     func addTask(_ title: String, _ text: String, _ personCharge: String, color: Color) {
