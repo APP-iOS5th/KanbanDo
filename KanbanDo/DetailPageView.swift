a30f918a8d768931b40d963bf07298aa76955469
Seunghye Hwang <ferrydraw@naver.com>
2024-05-03 02:48:19 +0900

    클래스 자료형 수정

--- KanbanDo/DetailPageView.swift
+++ KanbanDo/DetailPageView.swift
@@ -9,10 +9,12 @@
 import SwiftUI
 
 struct DetailPageView: View {
-    @StateObject private var tasks = TaskList(taskName: "태스크 이름", taskDetails: """
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-""",
-                                          workStatus: .doing, personCharge: ["박영식"], deadline: Date())
+    @StateObject private var tasks: TaskForDetail
+    
+    init(taskName: String, taskDetails: String, workStatus: ProjectDo.projectStatus, personCharge: String, deadline: Date) {
+            let task = TaskForDetail(taskName: taskName, taskDetails: taskDetails, workStatus: workStatus, personCharge: personCharge, deadline: deadline)
+            _tasks = StateObject(wrappedValue: task)
+        }
     
     var body: some View {
         
@@ -24,7 +26,8 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
                         .fontWeight(.bold)
                         .padding(.vertical)
                     
-                    TextField("설명을 입력하세요.", text: $tasks.taskDetails, axis: .vertical)
+                    //TextField("설명을 입력하세요.", text: $tasks.taskDetails, axis: .vertical)
+                    Text(tasks.taskDetails)
                         .padding()
                         .frame(maxWidth: .infinity, maxHeight: .infinity)
                         .background(.gray.opacity(0.1))
@@ -39,11 +42,10 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
                         .font(.title3)
                         .fontWeight(.bold)
                     Spacer()
-                    ForEach(tasks.personCharge, id: \.self) { person in
-                        Text(person)
+                        Text(tasks.personCharge)
                             .padding(.vertical)
                             .frame(alignment: .trailing)
-                    }
+                    
                 }
                 HStack {
                     Text("마감일")
@@ -64,9 +66,9 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
                         .padding(.vertical)
                     VStack {
                         Picker("", selection: $tasks.workStatus) {
-                            Text("할 일").tag(Status.notStarted)
-                            Text("진행 중").tag(Status.doing)
-                            Text("완료").tag(Status.done)
+                            Text("할 일").tag(ProjectDo.projectStatus.workDo)
+                            Text("진행 중").tag(ProjectDo.projectStatus.doing)
+                            Text("완료").tag(ProjectDo.projectStatus.done)
                         }
                     }
                 }
@@ -79,6 +81,6 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
 
 #Preview {
     NavigationStack {
-        DetailPageView()
+        DetailPageView(taskName: "새로운 태스크", taskDetails: "새로운 태스크 설명", workStatus: .doing, personCharge: "이름", deadline: Date())
     }
 }
--- KanbanDo/TaskForDetail.swift
+++ KanbanDo/TaskForDetail.swift
@@ -7,18 +7,18 @@
 
 import Foundation
 
-enum Status { // task 상태
-    case notStarted // 시작 전
-    case doing // 진행 중
-    case done // 완료
-}
+//enum TaskStatus { // task 상태
+//    case notStarted // 시작 전
+//    case doing // 진행 중
+//    case done // 완료
+//}
 
-class TaskList: ObservableObject { /* 클래스명 충돌로 수정 */
-    var taskID = UUID()
+class TaskForDetail: ObservableObject { /* 클래스명 충돌로 수정 */
+    //var taskID = UUID()
     var taskName: String  // 태스크 이름
-    @Published var taskDetails: String // task 설명 저장할 문자열, 변경 가능
-    @Published var workStatus: Status // 상태 변경 가능
-    var personCharge: [String] // 담당자
+    var taskDetails: String // task 설명 저장할 문자열
+    var workStatus: ProjectDo.projectStatus // 상태 변경 가능
+    var personCharge: String // 담당자
     var deadline: Date
     
     var deadlineString: String {
@@ -30,7 +30,7 @@ class TaskList: ObservableObject { /* 클래스명 충돌로 수정 */
         }
     }
     
-    init(taskName: String, taskDetails: String,  workStatus: Status, personCharge: [String], deadline: Date) {
+    init(taskName: String, taskDetails: String, workStatus: ProjectDo.projectStatus, personCharge: String, deadline: Date) {
         self.taskName = taskName
         self.taskDetails = taskDetails
         self.workStatus = workStatus
